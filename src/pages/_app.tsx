677e2514549bf6f1715efa8d13c63f3f949a3c95
Skipperlla <oesmer437@gmail.com>
2022-05-24 19:00:58 +0300

    Title Eklendi

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import "moment/locale/tr";
 import "@utils/lib/icons";
 import "react-alice-carousel/lib/alice-carousel.css";
 
+import Head from 'next/head'
 import MainLayout from "@layout/MainLayout";
 import type { AppProps } from "next/app";
 import { CoinProvider } from "@utils/context/CoinContext";
@@ -11,6 +12,9 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <CoinProvider>
       <MainLayout>
+        <Head>
+          <title>Kripto Takip Uygulaması</title>
+        </Head>
         <Component {...pageProps} />
       </MainLayout>
     </CoinProvider>
